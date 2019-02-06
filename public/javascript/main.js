b6ee540754d1fa806ad4632e3bfb0c2b3bd62d61
Emanuel Inacio <emanuelinacio1@hotmail.com>
2019-02-05 19:24:53 -0700

    texting recent.js access to files in recent.html

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,40 +1,44 @@
-"use strict"
-const electron = require('electron');
-const { ipcRenderer} = electron;
+"use strict";
+const electron = require("electron");
+const { ipcRenderer } = electron;
 
-const results = document.getElementById('right-side-container__results');
-let links = document.getElementsByClassName('link');
+const results = document.getElementById("right-side-container__results");
+let links = document.getElementsByClassName("link");
 
-for (let i = 0; i < links.length; i++){
-    links[i].addEventListener('click', (e)=>{
-        const class_name = e.target.className.split(" ")[0];
-        fetch(`../views/${class_name}.html`)
-        .then(res=>res.text())
-        .then(res=>{
-            results.innerHTML = res;
-        })
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", e => {
+    const class_name = e.target.className.split(" ")[0];
+    fetch(`../views/${class_name}.html`)
+      .then(res => res.text())
+      .then(res => {
+        results.innerHTML = res;
 
-    });
+        //Call recents wrapper to make content in recent.html available in recents.js
+        recents_wrapper();
+      });
+  });
 }
 
-
-function initMap(){
-    //Catch mission name sent from main.js entry file
-    ipcRenderer.on('create:newMission', (event, mission_name)=>{
+function initMap() {
+  //Catch mission name sent from main.js entry file
+  ipcRenderer.on("create:newMission", (event, mission_name) => {
     let options = {
-        zoom: 2,
-        center: {lat: 40.4637, lng: 3.7492}
-    }
+      zoom: 2,
+      center: { lat: 40.4637, lng: 3.7492 }
+    };
 
     let map = new google.maps.Map(results, options);
-    let marker = new google.maps.Marker({position: {lat: 40.4637, lng: 3.7492}, map: map});
+    let marker = new google.maps.Marker({
+      position: { lat: 40.4637, lng: 3.7492 },
+      map: map
+    });
 
-    marker.addListener('click', ()=>{
-        let contentString = '<h1> Lynn MA</h1>';
-        let infowindow = new google.maps.infoWindow({
-            content: contentString
-        });
-        infowindow.open(map, marker);
+    marker.addListener("click", () => {
+      let contentString = "<h1> Lynn MA</h1>";
+      let infowindow = new google.maps.infoWindow({
+        content: contentString
+      });
+      infowindow.open(map, marker);
     });
-    });
+  });
 }
---
+++ public/javascript/recents.js
@@ -0,0 +1,8 @@
+function recents_wrapper() {
+  "use strict";
+  // All code in this file should stay here
+
+  //Now you can access anything from recents.html
+  const recents = document.getElementById("recents");
+  recents.style.background = "gray";
+}
