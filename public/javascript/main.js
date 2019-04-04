5a675b51902706e0f4ddd71533c978c339343687
isaacfimbres1 <isaacfimbres1@yahoo.com>
2019-04-04 16:03:21 -0700

    Video streaming working

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -12,9 +12,24 @@ for (let i = 0; i < links.length; i++) {
       .then(res => res.text())
       .then(res => {
         results.innerHTML = res;
+        console.log(class_name)
+        //ipcRenderer.send(`load:${class_name}`);
+
+        
+        
+        //Couldn't think of another way, this is to call the wrapper function in each js
+        // var arr = class_name.split('-');
+        // var wrapper = '';
+        // arr.forEach((word) => {
+        //   wrapper += word;
+        // });
+        // console.log(wrapper)
+        // var wrapperFunc = new Function(`${wrapper}_wrapper`);
+        // wrapperFunc();
 
         //Call recents wrapper to make content in recent.html available in recents.js
-        recents_wrapper();
+        //recents_wrapper();
+        live_video_wrapper();
       });
   });
 }
